87cc2d872ea0bb94f8c347542b02071fae6e779c
cgew85 <christoph.gewehr@mailbox.org>
2016-04-16 18:49:48 +0200

    Added /v0/create

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # publisher-microservice
+
+A **simple microservice** for the **speedata publishing server**.
+It tries to _simplify_ the _usage_ of the publishers **REST** api
+by providing an own api.
--- src/main/java/io/github/cgew85/controller/PublisherRestController.java
+++ src/main/java/io/github/cgew85/controller/PublisherRestController.java
@@ -1,20 +1,21 @@
 package io.github.cgew85.controller;
 
-import elemental.json.JsonString;
 import io.github.cgew85.domain.Info;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * Created by cgew85 on 13.04.2016.
@@ -22,19 +23,36 @@ import java.io.InputStreamReader;
 @RestController
 public class PublisherRestController {
 
-    @RequestMapping("/v0/available")
+    /**
+     * Checks the availability of a given publishing server.
+     *
+     * @param address   -   The address of the publishing server.
+     * @param port      -   The port of the publishing server.
+     * @return          -   A string object containing the http status code.
+     */
+    @RequestMapping(value = "/v0/available", method = RequestMethod.GET)
     public String available(@RequestParam(value = "address") String address, @RequestParam(value = "port") String port) {
         return getJsonObjectFromHttpGetRequest(new HttpGet("http://" + address + ":" + port + "/available")).toJSONString();
     }
 
-    @RequestMapping("/v0/create")
+    @RequestMapping(value = "/v0/create", method = RequestMethod.POST)
     public String create(@RequestParam(value = "address") String address, @RequestParam(value = "port") String port,
-                         @RequestParam(value = "dataXml") JsonString dataXml, @RequestParam(value = "layoutXml") JsonString layoutXml) {
+                         @RequestParam(value = "dataXml") String dataXml, @RequestParam(value = "layoutXml") String layoutXml) {
         final CloseableHttpClient closeableHttpClient = HttpClients.createMinimal();
         final HttpPost httpPost = new HttpPost("http://" + address + ":" + port + "/v0/publish");
-        httpPost.addHeader("data.xml", dataXml.getString());
-        httpPost.addHeader("layout.xml", layoutXml.getString());
         final JSONObject jsonObject = new JSONObject();
+        try {
+            final StringEntity stringEntity = new StringEntity(
+                    "{\"layout.xml\":\"" + layoutXml.replace(" ", "+")
+                            + "\", \"data.xml\":\"" + new String(dataXml.replace(" ", "+").getBytes(), "UTF-8") + "\"}");
+            stringEntity.setContentType("application/json");
+            final StringWriter writer = new StringWriter();
+            IOUtils.copy(stringEntity.getContent(), writer, "UTF-8");
+            httpPost.setEntity(stringEntity);
+        } catch(Exception e) {
+            jsonObject.put("id", -1);
+            return jsonObject.toJSONString();
+        }
         try(CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost)) {
             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(closeableHttpResponse.getEntity().getContent()));
             final StringBuilder stringBuilder = new StringBuilder();
@@ -50,17 +68,27 @@ public class PublisherRestController {
         }
     }
 
-    @RequestMapping("/v0/info")
-    public Info info(@RequestParam(value="id") String id) {
+    @RequestMapping(value = "/v0/info", method = RequestMethod.GET)
+    public Info info(@RequestParam(value = "address") String address, @RequestParam(value = "port") String port,
+                     @RequestParam(value="id") String id) {
         return new Info("processId", "errorStatus", "result", "message", "finished");
     }
 
-    @RequestMapping("/v0/download")
-    public String download(@RequestParam(value="id") String id) {
+    @RequestMapping(value = "/v0/download", method = RequestMethod.GET)
+    public String download(@RequestParam(value = "address") String address, @RequestParam(value = "port") String port,
+                           @RequestParam(value="id") String id) {
         return "not available right now";
     }
 
-    @RequestMapping("/v0/delete")
+    /**
+     * Deletes a certain folder for a given id on a given publishing server.
+     *
+     * @param address   -   The address of the publisher.
+     * @param port      -   The port of the publisher.
+     * @param id        -   The id for the publishing process.
+     * @return          -   A string object containing the http status code.
+     */
+    @RequestMapping(value = "/v0/delete", method = RequestMethod.GET)
     public String delete(@RequestParam(value = "address") String address, @RequestParam(value = "port") String port,
                          @RequestParam(value="id") String id) {
         return getJsonObjectFromHttpGetRequest(new HttpGet("http://" + address + ":" + port + "/v0/delete/" + id)).toJSONString();
